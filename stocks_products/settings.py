6c42957e409c67a3a869dec98c3951483639e8e6
py-46-Alex <90619667+py-46-Alex@users.noreply.github.com>
2023-02-21 10:19:25 +0300

    Update settings.py

--- stocks_products/settings.py
+++ stocks_products/settings.py
@@ -75,7 +75,7 @@ WSGI_APPLICATION = 'stocks_products.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': os.getenv('DB_ENGINE'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': os.getenv('DB_NAME'),
         'HOST': os.getenv('DB_HOST'),
         'PORT': os.getenv('DB_PORT'), 'USER': os.getenv('DB_USER'), 'PASSWORD': os.getenv('DB_PASSWORD')
